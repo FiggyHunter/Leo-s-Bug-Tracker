9a1c50b6664e3c9600ab346435320d5487b36389
FiggyHunter <leonardo.roi3@gmail.com>
2023-11-06 17:17:37 +0100

    Added register form types

---
+++ src/types/RegisterForm.ts
@@ -0,0 +1,11 @@
+export type RegisterFormData = {
+  email: string;
+  password: string;
+  repeatPassword: string;
+};
+
+export type RegisterErrorData = {
+  email: null | string;
+  password: null | string;
+  repeatPassword: null | string;
+};
