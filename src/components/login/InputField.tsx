77b8114e2bf60b7da52049a4239bc9f8359abc22
FiggyHunter <leonardo.roi3@gmail.com>
2023-11-06 17:16:40 +0100

    type changes on the inputField

--- src/components/login/InputField.tsx
+++ src/components/login/InputField.tsx
@@ -3,7 +3,7 @@ interface InputFieldProps {
   name: string;
   type: string;
   handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  error: { [key: string]: string | null } | null;
+  error: null | string;
 }
 
 const InputField: React.FC<InputFieldProps> = ({
@@ -21,14 +21,12 @@ const InputField: React.FC<InputFieldProps> = ({
 
   return (
     <>
-      {error && (
-        <label className="text-red font-onest text-md">{error.message}</label>
-      )}
+      {error && <label className="text-red font-onest text-md">{error}</label>}
       <div
         className={`my-2 focus-within:border-accent-1 text-black bg-transparentFill gap-2  border-2 px-4 lg:py-4 py-2 rounded-xl flex justify-items-center ${ErrorStyles.button.borderColor}`}
       >
         {name === "email" && <img src="/mail.svg" alt="email icon" />}
-        {(name === "password" || name === "repeat-password") && (
+        {(name === "password" || name === "repeatPassword") && (
           <img src="/lock.svg" alt="password icon" />
         )}
         <input
