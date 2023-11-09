acc18b3232f87a4ebbdf8d21027c2d0857210e5b
FiggyHunter <leonardo.roi3@gmail.com>
2023-11-09 17:32:28 +0100

    button component for navigation only

---
+++ src/components/shared/ButtonNavigation.tsx
@@ -0,0 +1,33 @@
+import { Path, useNavigate } from "react-router-dom";
+
+interface ButtonNavigationProps {
+  text: String;
+  route: Partial<Path> | string;
+  styleType: String;
+}
+
+const ButtonNavigation: React.FC<ButtonNavigationProps> = ({
+  text,
+  route,
+  styleType,
+}) => {
+  const navigate = useNavigate();
+  const handleButtonClick = () => {
+    navigate(route);
+  };
+
+  return (
+    <button
+      onClick={handleButtonClick}
+      className={`px-5 py-3 my-4 font-onest font-bold text-accent-1 bg-transparent  ${
+        styleType === "secondary"
+          ? "hover:font-black"
+          : "border-accent-1 border-2  hover:text-black hover:bg-accent-1   duration-250 hover:scale-105 transition-all duration-250"
+      } `}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default ButtonNavigation;
