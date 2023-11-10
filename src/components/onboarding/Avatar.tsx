8defcaf05863b5a5002da484067d2e269dba38fa
FiggyHunter <leonardo.roi3@gmail.com>
2023-11-10 23:53:40 +0100

    handle Avatar display

--- src/components/onboarding/Avatar.tsx
+++ src/components/onboarding/Avatar.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import { MouseEvent } from "react";
 
 interface Props {
+  active: boolean;
+  setter: (event: MouseEvent<HTMLButtonElement>) => void;
   imageUrl: String;
 }
 
-const Avatar: React.FC<Props> = ({ imageUrl }) => {
+const Avatar: React.FC<Props> = ({ imageUrl, active, setter }) => {
   return (
-    <button className="w-full bg-accent-1 h-full ">
-      <img src={`/${imageUrl}`} />
+    <button
+      onClick={setter}
+      className={`relative transition-all duration-200 w-5/6 overflow-hidden bg-accent-1 ${
+        active ? "scale-75" : " "
+      }`}
+    >
+      <img className="relative w-full" src={`/${imageUrl}`} />
     </button>
   );
 };
