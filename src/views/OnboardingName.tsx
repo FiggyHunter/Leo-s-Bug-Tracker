0c8618fa1c4b21195363f3252804c160558fe5cb
FiggyHunter <leonardo.roi3@gmail.com>
2023-11-10 23:55:48 +0100

    worked on second part of onboarding

--- src/views/OnboardingName.tsx
+++ src/views/OnboardingName.tsx
@@ -2,8 +2,10 @@ import { useState } from "react";
 
 import Button from "@/components/shared/Button";
 import { validateName } from "@/utils/validators/validateName";
+import { useNavigate } from "react-router-dom";
 
 const OnboardingName = () => {
+  const navigate = useNavigate();
   const [namePreference, setNamePreference] = useState("");
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setNamePreference(event.target.value);
@@ -17,22 +19,23 @@ const OnboardingName = () => {
     e.preventDefault();
     try {
       await validateName(namePreference, setNameError);
+      navigate("/dashboard");
     } catch (error) {
       console.log(error);
     }
   };
 
   return (
-    <div className="grid grid-cols-1  content-center justify-items-center h-full w-full">
-      <form className="grid">
+    <div className="grid grid-cols-1  justify-items-center h-screen w-screen">
+      <form className="grid self-center">
         <input
-          className="text-5xl text-center text-white placeholder-gray-400 font-onest font-bold bg-transparent border-b-2 border-b-white focus:border-b-accent-1 focus:outline-none"
+          className="text-xl lg:text-5xl text-center text-white placeholder-gray-400 font-onest font-bold bg-transparent border-b-2 border-b-white focus:border-b-accent-1 focus:outline-none"
           value={namePreference} // Bind the input value to your state
           onChange={handleChange}
           type="text"
           placeholder="ENTER YOUR NAME"
         />
-        <label className="font-semibold text-lg font-onest text-red text-center mt-4">
+        <label className="font-semibold text-lg lg:text-2xl font-onest text-red text-center mt-4">
           {nameError}
         </label>
         <div className="text-center w-1/2 mx-auto">
