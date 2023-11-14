920942fd5461a64b6bdc6f7fe051d7ce5d34fbe2
FiggyHunter <leonardo.roi3@gmail.com>
2023-11-14 21:47:50 +0100

    worked on navigation

--- src/components/shared/Navigation.tsx
+++ src/components/shared/Navigation.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 
-const Navigation = () => {
+interface Props {
+  avatar: string | undefined;
+}
+
+const Navigation: React.FC<Props> = ({ avatar }) => {
   return (
-    <nav className="w-full p-3 font-onest items-center  grid grid-cols-2">
+    <nav className="w-full px-7 py-3 font-onest items-center  grid grid-cols-2">
       <p className="hover:cursor-pointer text-accent-1  font-bold md:text-xl lg:text-3xl ml-2 block">
         LEO'S BUG TRACKER
       </p>
@@ -12,10 +16,7 @@ const Navigation = () => {
       >
         <img className="w-5 h-5 lg:w-7 lg:h-7" src="/bell.svg" />
         <button className="rounded-full overflow-hidden">
-          <img
-            className="bg-blue-300 w-5 h-5 lg:w-10 lg:h-10"
-            src="/panda.webp"
-          />
+          <img className="bg-blue-300 w-5 h-5 lg:w-10 lg:h-10" src={avatar} />
         </button>
       </div>
     </nav>
