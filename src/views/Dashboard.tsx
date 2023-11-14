c487b88522766a9e3b47dc2fe58e604da28b1701
FiggyHunter <leonardo.roi3@gmail.com>
2023-11-14 21:48:10 +0100

    worked on dashboard, fetching the jwt

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,17 +1,36 @@
 import DashboardContent from "@/components/dashboard/DashboardContent";
 import Navigation from "@/components/shared/Navigation";
-import React from "react";
+import { useCustomStore } from "@/store/useStore";
+import { useEffect, useState } from "react";
+import { useJwt } from "react-jwt";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
+  const { jwt } = useCustomStore();
+  const navigate = useNavigate();
+
+  const [user, setUser] = useState({});
+  const token = useJwt(jwt) || null; // Directly use the token
+
+  console.log(token);
+
+  useEffect(() => {
+    if (token.isExpired) {
+      navigate("/login");
+      return;
+    }
+    setUser(token.decodedToken);
+  }, [token, navigate]);
+
   return (
-    <>
+    <div className="h-my-screen grid custom-rows-2 grid-cols-1">
       <header>
-        <Navigation />
+        <Navigation avatar={`/${user?.Avatar}`} />
       </header>
-      <main>
-        <DashboardContent />
+      <main className="self-center">
+        <DashboardContent userName={user?.Name} />
       </main>
-    </>
+    </div>
   );
 };
 
