376c1da15f7c4b6ff5c97b4b8e5c0f635138504c
FiggyHunter <leonardo.roi3@gmail.com>
2023-11-19 23:34:36 +0100

    error handling onboarding on UI

--- src/views/Onboarding.tsx
+++ src/views/Onboarding.tsx
@@ -6,6 +6,7 @@ import ButtonNavigation from "../components/shared/ButtonNavigation";
 import usePreferences from "@/hooks/usePreferences";
 import { AvatarsState, RolesState } from "@/types/Preferences";
 import Button from "@/components/shared/Button";
+import { useNavigate } from "react-router-dom";
 
 const Onboarding = () => {
   const {
@@ -17,7 +18,10 @@ const Onboarding = () => {
     setAvatar,
     setRoles,
     handlePreferenceUpdate,
+    userPreferenceErrors,
   } = usePreferences();
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     setUserPreference((prevPreference: any) => {
@@ -71,7 +75,11 @@ const Onboarding = () => {
   return (
     <main className="grid grid-rows-2 md:h-my-screen lg:h-my-screen   gap-5 lg:grid lg:grid-cols-2 lg:grid-rows-1 w-5/6 mx-auto cursor-default content-between ">
       <div className="flex flex-col gap-3 lg:h-5/6 lg:self-center ">
-        <h1 className="text-xl sm:text-center lg:text-left font-onest font-bold w-full text-content mt-2 md:text-3xl">
+        <h1
+          className={`${
+            userPreferenceErrors.role === true ? "text-red" : "text-content"
+          } text-xl sm:text-center lg:text-left font-onest font-bold w-full text-content mt-2 md:text-3xl`}
+        >
           SELECT YOUR ROLE
         </h1>
         <section className="flex h-4/6 gap-3">
@@ -96,7 +104,11 @@ const Onboarding = () => {
         </div>
       </div>
       <div className="flex flex-col h-5/6 lg:my-auto gap-3 ">
-        <h1 className="text-xl sm:text-center lg:text-left lg:hidden font-onest w-full text-content lg:order-2 lg:w-1/3 lg:self-end lg:h-12 place-content-center md:text-3xl">
+        <h1
+          className={`${
+            userPreferenceErrors.avatar === true ? "text-red" : "text-content"
+          } text-xl sm:text-center lg:text-left lg:hidden font-onest w-full text-content lg:order-2 lg:w-1/3 lg:self-end lg:h-12 place-content-center md:text-3xl`}
+        >
           SELECT AN AVATAR
         </h1>
         <div className="grid sm:grid-cols-2 sm:grid-rows-2 sm:items-center gap-3 md:grid-cols-4 md:grid-rows-none lg:grid-cols-2 md:grid-flow-dense w-full md:h-1/3 lg:h-full lg:custom-rows lg:place-content-center place-items-center md:overflow-hidden ">
@@ -114,11 +126,18 @@ const Onboarding = () => {
           <Button text="CONTINUE" handler={handlePreferenceUpdate} />
         </div>
         <div className="hidden h-1/6 lg:flex px-5">
-          <h1 className="font-onest font-bold w-full text-content lg:order-2 lg:w-2/3 lg:self-end lg:h-12 lg:grid content-center lg:text-2xl text-left">
+          <h1
+            className={`${
+              userPreferenceErrors.avatar === true ? "text-red" : "text-content"
+            } font-onest font-bold w-full text-content lg:order-2 lg:w-2/3 lg:self-end lg:h-12 lg:grid content-center lg:text-2xl text-left`}
+          >
             SELECT AN AVATAR
           </h1>
           <div className="lg:h-12 lg:order-3   lg:w-1/3  lg:self-end lg:ml-auto grid content-center hover:scale-95 transition-transform duration-250 ">
-            <Button text="CONTINUE" handler={handlePreferenceUpdate} />
+            <Button
+              text="CONTINUE"
+              handler={() => handlePreferenceUpdate(navigate)}
+            />
           </div>
         </div>
       </div>
