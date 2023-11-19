769a197de9477e3df846861fb72bcf4da0a9a361
FiggyHunter <leonardo.roi3@gmail.com>
2023-11-19 16:57:04 +0100

    refactored onboarding component

--- src/views/Onboarding.tsx
+++ src/views/Onboarding.tsx
@@ -1,65 +1,33 @@
 import Role from "@/components/onboarding/Role";
 import Avatar from "@/components/onboarding/Avatar";
 import { useJwt } from "react-jwt";
-import { useState } from "react";
+import { useEffect } from "react";
 import ButtonNavigation from "../components/shared/ButtonNavigation";
-
-interface Role {
-  roleName: string;
-  active: boolean;
-}
-
-interface RolesState {
-  developer: Role;
-  qa: Role;
-  generalist: Role;
-}
-
-interface Avatar {
-  label: string;
-  imageUrl: string;
-  active: boolean;
-}
-
-interface AvatarsState {
-  panda: Avatar;
-  owl: Avatar;
-  fox: Avatar;
-  dolphin: Avatar;
-}
+import usePreferences from "@/hooks/usePreferences";
+import { AvatarsState, RolesState } from "@/types/Preferences";
+import Button from "@/components/shared/Button";
 
 const Onboarding = () => {
-  // const tokenDecoded: null = useJwt(localStorage.getItem("token")) || null;
-  // console.log(tokenDecoded);
+  const {
+    avatar,
+    roles,
+    setUserPreference,
+    findActiveAvatar,
+    findActiveRole,
+    setAvatar,
+    setRoles,
+    handlePreferenceUpdate,
+  } = usePreferences();
 
-  const [roles, setRoles] = useState<RolesState>({
-    developer: { roleName: "Developer", active: false },
-    qa: { roleName: "QA Tester", active: false },
-    generalist: { roleName: "Generalist", active: false },
-  });
-
-  const [avatar, setAvatar] = useState<AvatarsState>({
-    panda: {
-      label: "panda",
-      imageUrl: "panda.webp",
-      active: false,
-    },
-    owl: {
-      label: "owl",
-      imageUrl: "owl.webp",
-      active: false,
-    },
-    fox: {
-      label: "fox",
-      imageUrl: "fox.webp",
-      active: false,
-    },
-    dolphin: {
-      label: "dolphin",
-      imageUrl: "dolphin.webp",
-      active: false,
-    },
-  });
+  useEffect(() => {
+    setUserPreference((prevPreference: any) => {
+      return {
+        ...prevPreference,
+        role: findActiveRole(roles),
+        avatar: findActiveAvatar(avatar),
+      };
+    });
+  }, [avatar, roles]);
 
   const updateAvatar = (imageName: string) => {
     setAvatar((prevAvatars: AvatarsState) => {
@@ -101,7 +69,7 @@ const Onboarding = () => {
   };
 
   return (
-    <main className="grid grid-rows-2 h-screen  gap-5 lg:grid lg:grid-cols-2 lg:grid-rows-1 w-5/6 mx-auto cursor-default content-between ">
+    <main className="grid grid-rows-2 md:h-my-screen lg:h-my-screen   gap-5 lg:grid lg:grid-cols-2 lg:grid-rows-1 w-5/6 mx-auto cursor-default content-between ">
       <div className="flex flex-col gap-3 lg:h-5/6 lg:self-center ">
         <h1 className="text-xl sm:text-center lg:text-left font-onest font-bold w-full text-content mt-2 md:text-3xl">
           SELECT YOUR ROLE
@@ -131,7 +99,7 @@ const Onboarding = () => {
         <h1 className="text-xl sm:text-center lg:text-left lg:hidden font-onest w-full text-content lg:order-2 lg:w-1/3 lg:self-end lg:h-12 place-content-center md:text-3xl">
           SELECT AN AVATAR
         </h1>
-        <div className="grid sm:grid-cols-2 sm:grid-rows-2 sm:items-center gap-3 md:grid-cols-4 md:grid-rows-none lg:grid-cols-2 md:grid-flow-dense w-full md:h-1/3 lg:h-full lg:custom-rows lg:place-content-center place-items-center ">
+        <div className="grid sm:grid-cols-2 sm:grid-rows-2 sm:items-center gap-3 md:grid-cols-4 md:grid-rows-none lg:grid-cols-2 md:grid-flow-dense w-full md:h-1/3 lg:h-full lg:custom-rows lg:place-content-center place-items-center md:overflow-hidden ">
           {avatar &&
             Object.values(avatar).map((avatar) => (
               <Avatar
@@ -143,14 +111,14 @@ const Onboarding = () => {
             ))}
         </div>
         <div className=" my-4 lg:hidden  h-min  lg:order-3 font-onest  lg:w-1/3  lg:ml-auto grid place-self-center w-2/4">
-          <ButtonNavigation text="CONTINUE" route={"/onboarding-name"} />
+          <Button text="CONTINUE" handler={handlePreferenceUpdate} />
         </div>
         <div className="hidden h-1/6 lg:flex px-5">
           <h1 className="font-onest font-bold w-full text-content lg:order-2 lg:w-2/3 lg:self-end lg:h-12 lg:grid content-center lg:text-2xl text-left">
             SELECT AN AVATAR
           </h1>
           <div className="lg:h-12 lg:order-3   lg:w-1/3  lg:self-end lg:ml-auto grid content-center hover:scale-95 transition-transform duration-250 ">
-            <ButtonNavigation text="CONTINUE" route={"/onboarding-name"} />
+            <Button text="CONTINUE" handler={handlePreferenceUpdate} />
           </div>
         </div>
       </div>
