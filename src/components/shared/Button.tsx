173106755e1e93a0c9cc0e8025c9dff0825f0a94
FiggyHunter <leonardo.roi3@gmail.com>
2023-11-21 22:06:39 +0100

    added loading state for button

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -1,15 +1,30 @@
+import { useButtonLoadingStore } from "@/store/useButtonLoadingStore";
+import { useState } from "react";
+
 interface ButtonProps {
   text: string;
   handler: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 const Button: React.FC<ButtonProps> = ({ text, handler }) => {
+  const { buttonLoading: isLoading } = useButtonLoadingStore();
+
   return (
     <button
       className="mt-8 mb-4 mx-auto block text-content border-accent-1 border-2 w-full py-2 rounded-xl font-onest font-medium tracking-wider focus:bg-accent-1 focus:text-bkg focus:outline-none    hover:bg-accent-1 hover:text-bkg transition-all duration-300 lg:py-4"
       onClick={handler}
     >
-      {text}
+      <div
+        className={` ${
+          isLoading ? "block" : "hidden"
+        } inline-block h-4 w-4 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]`}
+        role="status"
+      >
+        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+          Loading...
+        </span>
+      </div>
+      <p className={`${isLoading ? "hidden" : "block"}`}>{text}</p>
     </button>
   );
 };
