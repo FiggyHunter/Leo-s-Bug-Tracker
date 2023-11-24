067dd61bb42fde498a9e3b64cd9afea5099038b1
FiggyHunter <leonardo.roi3@gmail.com>
2023-11-24 18:04:20 +0100

    style adjustments on project

--- src/components/dashboard/Project.tsx
+++ src/components/dashboard/Project.tsx
@@ -7,12 +7,11 @@ interface Props {
 
 const Project: React.FC<Props> = ({ projectTitle, projectDescription }) => {
   return (
-    <div className="pb-8">
+    <div className="pb-8 text-center lg:text-left">
       <a className="text-accent-1 text-2xl font-onest font-extrabold uppercase ">
         {projectTitle}
       </a>
-      <p className="text-content font-onest w-1/2 text-xl">
-        {" "}
+      <p className="text-content font-onest md:w-1/2 font-light w-5/6 mx-auto lg:mx-0 text-xl">
         {projectDescription}
       </p>
     </div>
