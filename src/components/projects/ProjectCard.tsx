335591af85cb6bdcb75253ee9bd963905b6fa8c5
FiggyHunter <leonardo.roi3@gmail.com>
2023-11-28 23:47:11 +0100

    style adjustments

--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const ProjectCard = ({ color }) => {
   return (
     <article>
-      <div className={`md:h-96 sm:h-36 w-full cursor-pointer ${color}`}></div>
-      <h3 className="text-content text-2xl font-onest font-medium cursor-pointer sm:text-base mt-3">
+      <div className={`md:h-64 sm:h-36 w-full cursor-pointer ${color}`}></div>
+      <h3 className="text-content md:text-xl text-2xl font-onest font-medium cursor-pointer sm:text-base mt-3">
         Project name bla bla blah Project name bla bla blah.
       </h3>
     </article>
