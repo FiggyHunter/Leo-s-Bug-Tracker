e548bcd79813ac9c91cf8a090a04b99e5eb97fab
FiggyHunter <leonardo.roi3@gmail.com>
2023-11-30 23:10:52 +0100

    Project route component implementation

---
+++ src/views/Project.tsx
@@ -0,0 +1,17 @@
+import ProjectContent from "@/components/project/ProjectContent";
+import Navigation from "@/components/shared/Navigation";
+
+const Project = () => {
+  return (
+    <>
+      <header>
+        <Navigation />
+      </header>
+      <>
+        <ProjectContent />
+      </>
+    </>
+  );
+};
+
+export default Project;
