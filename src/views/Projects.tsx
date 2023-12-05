e046f7186d879680f7600756ff20bfc0e30080f5
FiggyHunter <leonardo.roi3@gmail.com>
2023-12-05 23:30:44 +0100

    small refactor

--- src/views/Projects.tsx
+++ src/views/Projects.tsx
@@ -8,39 +8,18 @@ import ProjectsContent from "@/components/projects/ProjectsContent";
 const Projects = () => {
   const { jwt } = useCustomStore();
   const navigate = useNavigate();
-
   const [user, setUser] = useState({});
-
   const token = useJwt(jwt) || null; // Directly use the token
 
-  console.log(token);
-
-  // useEffect(() => {
-  //   if (token.isExpired) {
-  //     navigate("/login");
-  //     return;
-  //   }
-  //   if (token.decodedToken !== null)
-  //   {
-  //     if (
-  //       token.decodedToken.Role === "unset" ||
-  //       token.decodedToken.Avatar ==="unset"
-
-  //     ) {
-  //       navigate("/onboarding");
-  //       return;
-  //     }
-  //     if(token.decodedToken.Name === "unset") {
-  //       navigate("/onboarding-name");
-  //       return;
-  //     }
-  //   setUser(token.decodedToken);
-  //   }
-  // }, [token, navigate]);
   return (
     <div className="h-my-screen grid custom-rows-2 grid-cols-1">
       <header>
-        <Navigation avatar={`/${user?.Avatar}`} />
+        <Navigation
+          navigate={() => {
+            navigate("/dashboard");
+          }}
+          avatar={`/${user?.Avatar}`}
+        />
       </header>
       <main className="mt-8 mb-12 w-10/12 mx-auto px-4">
         <ProjectsContent />
