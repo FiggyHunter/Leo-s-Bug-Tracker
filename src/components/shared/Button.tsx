d3865761ea8d8cd951dbc675e2f4f676b86dd121
FiggyHunter <leonardo.roi3@gmail.com>
2023-12-08 23:56:02 +0100

    adjusted button for new id based loading

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -1,18 +1,19 @@
 import { useButtonLoadingStore } from "@/store/useButtonLoadingStore";
-import { useState } from "react";
 
 interface ButtonProps {
   text: string;
-  handler: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  buttonId: string;
+  handler: (e: React.MouseEvent<HTMLButtonElement>, buttonId: string) => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ text, handler }) => {
-  const { buttonLoading: isLoading } = useButtonLoadingStore();
+const Button: React.FC<ButtonProps> = ({ text, buttonId, handler }) => {
+  const { buttonLoading } = useButtonLoadingStore();
+  const isLoading = buttonLoading[buttonId] || false;
 
   return (
     <button
       className="mt-8 mb-4 mx-auto block text-content border-accent-1 border-2 w-full py-2 rounded-xl font-onest font-medium tracking-wider focus:bg-accent-1 focus:text-bkg focus:outline-none    hover:bg-accent-1 hover:text-bkg transition-all duration-300 lg:py-4"
-      onClick={handler}
+      onClick={(e) => handler(e, buttonId)}
     >
       <div
         className={` ${
