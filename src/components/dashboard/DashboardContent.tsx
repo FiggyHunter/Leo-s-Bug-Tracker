555742e7e7730cfcfeadb8765a89c1962526fbd7
FiggyHunter <leonardo.roi3@gmail.com>
2023-12-10 20:19:19 +0100

    project rendering now dynamic

--- src/components/dashboard/DashboardContent.tsx
+++ src/components/dashboard/DashboardContent.tsx
@@ -1,6 +1,8 @@
 import { useNavigate } from "react-router-dom";
 import Bug from "./Bug";
 import Project from "./Project";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 interface Props {
   userName: string | null;
@@ -8,12 +10,29 @@ interface Props {
 
 const DashboardContent: React.FC<Props> = ({ userName }) => {
   const navigate = useNavigate();
+  const [projects, setProjects] = useState({});
+
+  const fetchProjects = async () => {
+    const fetchedProjects = await axios.get("http://localhost:4000/project", {
+      headers: {
+        "X-USER-ID": "creatorUserId",
+      },
+    });
+
+    setProjects(fetchedProjects.data.projects);
+    console.log(projects);
+  };
+
+  useEffect(() => {
+    fetchProjects();
+  }, []);
+
   return (
     <>
       <h1 className="hover:cursor-default text-center font-black text-content font-onest text-3xl w-4/5 mx-auto leading-tight lg:py-5 py-10 lg:text-6xl lg:w-full lg:pl-8">
         {userName}'s Dashboard
       </h1>
-      <section className="mt-10 hover:cursor-default  lg:grid lg:grid-cols-2 lg:px-8  ">
+      <section className="sm:mt-4 hover:cursor-default  lg:grid lg:grid-cols-2 lg:mt-10  lg:px-8  ">
         <article className="text-center lg:text-left ">
           <h2 className="text-3xl font-bold font-onest text-content lg:text-4xl mb-4 lg:mb-8 text-center lg:text-left ">
             YOUR RECENT BUGS:
@@ -56,18 +75,14 @@ const DashboardContent: React.FC<Props> = ({ userName }) => {
               SEE ALL
             </button>
           </div>
-          <Project
-            projectTitle={"A cool eco project."}
-            projectDescription={
-              "Mobile app to manage personal finances with real-time budget tracking, expenditure alerts, and customizable financial goals."
-            }
-          />
-          <Project
-            projectTitle={"A cool eco project."}
-            projectDescription={
-              "Mobile app to manage personal finances with real-time budget tracking, expenditure alerts, and customizable financial goals."
-            }
-          />
+
+          {Object.values(projects)?.map((project) => (
+            <Project
+              key={project._id} // Assuming you have a unique identifier for each project
+              projectTitle={project.projectName}
+              projectDescription={project.description}
+            />
+          ))}
         </div>
       </section>
     </>
