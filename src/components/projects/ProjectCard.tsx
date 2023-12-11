170884d86992fb88339ad4d36c553e6fb1e41dfb
FiggyHunter <leonardo.roi3@gmail.com>
2023-12-11 23:52:38 +0100

    started working on project card

--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -1,11 +1,14 @@
-import React from "react";
-import Bug from "../dashboard/Bug";
+import { useEffect, useRef, useState } from "react";
 
-const ProjectCard = ({ color }) => {
+const ProjectCard = ({ name, description, color }) => {
+  const divStyle = {
+    backgroundColor: color,
+  };
   return (
     <article>
       <div
-        className={`lg:h-96 md:h-60 sm:h-36 w-full cursor-pointer ${color} rounded-2xl`}
+        style={divStyle}
+        className={`lg:h-96 md:h-60 sm:h-36 w-full cursor-pointer  rounded-2xl`}
       ></div>
       <h3 className="text-content md:text-xl text-2xl font-onest font-medium cursor-pointer sm:text-base mt-3">
         Project name bla bla blah Project name bla bla blah.
