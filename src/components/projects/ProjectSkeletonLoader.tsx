0b465304bb0cc98ee55df9810271f46c32d0abac
FiggyHunter <leonardo.roi3@gmail.com>
2023-12-11 23:53:28 +0100

    custom skeleton loading for projects

---
+++ src/components/projects/ProjectSkeletonLoader.tsx
@@ -0,0 +1,194 @@
+const ProjectSkeletonLoader = () => {
+  return (
+    <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 grid-rows-1 gap-7 h-full w-full">
+      <div role="status" className="space-y-2.5 animate-pulse max-w-lg">
+        <div className="flex items-center  w-full mb-4">
+          <div className="lg:h-96 md:h-60 sm:h-36 bg-gray-300 rounded-lg dark:bg-gray-600 w-full"></div>
+        </div>
+        <div className="flex items-center w-full max-w-[480px]">
+          <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+        </div>
+        <div className="flex items-center w-full max-w-[400px]">
+          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-80"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+        </div>
+        <div className="flex items-center w-full max-w-[480px]">
+          <div className="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+        </div>
+        <div className="flex items-center w-full max-w-[440px]">
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-32"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+          <div className="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
+        </div>
+        <div className="flex items-center w-full max-w-[360px]">
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-80"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+        </div>
+        <span className="sr-only">Loading...</span>
+      </div>
+      <div role="status" className="space-y-2.5 animate-pulse max-w-lg">
+        <div className="flex items-center  w-full mb-4">
+          <div className="lg:h-96 md:h-60 sm:h-36 bg-gray-300 rounded-lg dark:bg-gray-600 w-full"></div>
+        </div>
+        <div className="flex items-center w-full max-w-[480px]">
+          <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+        </div>
+        <div className="flex items-center w-full max-w-[400px]">
+          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-80"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+        </div>
+        <div className="flex items-center w-full max-w-[480px]">
+          <div className="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+        </div>
+        <div className="flex items-center w-full max-w-[440px]">
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-32"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+          <div className="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
+        </div>
+        <div className="flex items-center w-full max-w-[360px]">
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-80"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+        </div>
+        <span className="sr-only">Loading...</span>
+      </div>{" "}
+      <div role="status" className="space-y-2.5 animate-pulse max-w-lg">
+        <div className="flex items-center  w-full mb-4">
+          <div className="lg:h-96 md:h-60 sm:h-36 bg-gray-300 rounded-lg dark:bg-gray-600 w-full"></div>
+        </div>
+        <div className="flex items-center w-full max-w-[480px]">
+          <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+        </div>
+        <div className="flex items-center w-full max-w-[400px]">
+          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-80"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+        </div>
+        <div className="flex items-center w-full max-w-[480px]">
+          <div className="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+        </div>
+        <div className="flex items-center w-full max-w-[440px]">
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-32"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+          <div className="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
+        </div>
+        <div className="flex items-center w-full max-w-[360px]">
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-80"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+        </div>
+        <span className="sr-only">Loading...</span>
+      </div>{" "}
+      <div role="status" className="space-y-2.5 animate-pulse max-w-lg">
+        <div className="flex items-center  w-full mb-4">
+          <div className="lg:h-96 md:h-60 sm:h-36 bg-gray-300 rounded-lg dark:bg-gray-600 w-full"></div>
+        </div>
+        <div className="flex items-center w-full max-w-[480px]">
+          <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+        </div>
+        <div className="flex items-center w-full max-w-[400px]">
+          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-80"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+        </div>
+        <div className="flex items-center w-full max-w-[480px]">
+          <div className="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+        </div>
+        <div className="flex items-center w-full max-w-[440px]">
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-32"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+          <div className="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
+        </div>
+        <div className="flex items-center w-full max-w-[360px]">
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-80"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+        </div>
+        <span className="sr-only">Loading...</span>
+      </div>
+      <div role="status" className="space-y-2.5 animate-pulse max-w-lg">
+        <div className="flex items-center  w-full mb-4">
+          <div className="lg:h-96 md:h-60 sm:h-36 bg-gray-300 rounded-lg dark:bg-gray-600 w-full"></div>
+        </div>
+        <div className="flex items-center w-full max-w-[480px]">
+          <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+        </div>
+        <div className="flex items-center w-full max-w-[400px]">
+          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-80"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+        </div>
+        <div className="flex items-center w-full max-w-[480px]">
+          <div className="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+        </div>
+        <div className="flex items-center w-full max-w-[440px]">
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-32"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+          <div className="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
+        </div>
+        <div className="flex items-center w-full max-w-[360px]">
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-80"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+        </div>
+        <span className="sr-only">Loading...</span>
+      </div>{" "}
+      <div role="status" className="space-y-2.5 animate-pulse max-w-lg">
+        <div className="flex items-center  w-full mb-4">
+          <div className="lg:h-96 md:h-60 sm:h-36 bg-gray-300 rounded-lg dark:bg-gray-600 w-full"></div>
+        </div>
+        <div className="flex items-center w-full max-w-[480px]">
+          <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+        </div>
+        <div className="flex items-center w-full max-w-[400px]">
+          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-80"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+        </div>
+        <div className="flex items-center w-full max-w-[480px]">
+          <div className="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+        </div>
+        <div className="flex items-center w-full max-w-[440px]">
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-32"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+          <div className="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
+        </div>
+        <div className="flex items-center w-full max-w-[360px]">
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          <div className="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-80"></div>
+          <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+        </div>
+        <span className="sr-only">Loading...</span>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectSkeletonLoader;
