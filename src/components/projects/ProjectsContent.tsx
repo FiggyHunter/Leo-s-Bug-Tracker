08529d86848cfca5b602e440a2553994cc0ea14e
FiggyHunter <leonardo.roi3@gmail.com>
2023-12-11 23:53:18 +0100

    adjusted loading of projects on all projects page

--- src/components/projects/ProjectsContent.tsx
+++ src/components/projects/ProjectsContent.tsx
@@ -1,19 +1,17 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useEffect } from "react";
 import ProjectCard from "./ProjectCard";
 import "../../styles/animations.css";
+import NewProjectModal from "./NewProjectModal";
+import { fetchProjects } from "@/api/projects/useProjects";
+import ProjectSkeletonLoader from "./ProjectSkeletonLoader";
 
 const ProjectsContent = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const animatedRef = useRef(null);
+  const [projects, setProjects] = useState({});
+  console.log(projects);
   useEffect(() => {
-    if (isModalOpen) {
-      // Trigger animation when component becomes visible
-      animatedRef?.current?.classList.add("modal-is-open");
-    } else {
-      // Reset animation when component becomes invisible
-      animatedRef?.current?.classList.remove("modal-is-open");
-    }
-  }, [isModalOpen]);
+    fetchProjects(setProjects);
+  }, []);
   return (
     <>
       <div className="sm:flex-wrap relative flex justify-between items-center">
@@ -26,32 +24,31 @@ const ProjectsContent = () => {
         >
           New Project
         </button>
-        {isModalOpen && (
-          <section
-            ref={animatedRef}
-            className="rounded-xl sm:top-full sm:w-5/6 sm:right-7 md:right-11  md:w-48   overflow-hidden absolute transition-all duration-500 right-0 top-3/4 font-onest text-bkg bg-content w-96 flex flex-col"
-          >
-            <p className="text-center p-4 text-lg">Project Name:</p>
-            <input
-              className="text-center"
-              placeholder="Type your project name here..."
-              type="text"
-            />
-            <input
-              className="mx-auto mt-2"
-              placeholder="Type your project name here..."
-              type="color"
-            />
-            <button className="m-4 mx-auto hover:scale-95 transition-transform duration-150 w-1/2 bg-accent-1 p-2">
-              Save Project
-            </button>
-          </section>
-        )}
+        {isModalOpen && <NewProjectModal isModalOpen={isModalOpen} />}
       </div>
-      <section className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 w-full gap-10 mt-7">
-        <ProjectCard color={"bg-red"} />
-        <ProjectCard color={"bg-blue-500"} />
-        <ProjectCard color={"bg-green-200"} />
+      <section
+        className={`grid ${
+          projects !== null && Object.keys(projects).length === 0
+            ? `grid-cols-1`
+            : `sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3`
+        }  w-full gap-10 mt-7`}
+      >
+        {projects === null ? (
+          <div className="bg-red text-2xl place-self-cente sm:col-span-2 md:col-span-2 lg:col-span-3">
+            No projects available.
+          </div>
+        ) : Object.keys(projects).length === 0 ? (
+          <ProjectSkeletonLoader />
+        ) : (
+          projects.map((project) => (
+            <ProjectCard
+              key={project._id}
+              name={project.projectName}
+              description={project.description}
+              color={project.color}
+            />
+          ))
+        )}
       </section>
     </>
   );
