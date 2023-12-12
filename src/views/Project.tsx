c4d638f93214f18af602f30fbfaff6d21f6c2ae5
FiggyHunter <leonardo.roi3@gmail.com>
2023-12-12 23:51:56 +0100

    worked on the project view

--- src/views/Project.tsx
+++ src/views/Project.tsx
@@ -1,14 +1,31 @@
+import { fetchProjectById } from "@/api/projects/projects";
 import ProjectContent from "@/components/project/ProjectContent";
 import Navigation from "@/components/shared/Navigation";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 const Project = () => {
+  const navigate = useNavigate();
+  const params = useParams();
+  const { projectId } = params;
+  const [project, setProject] = useState({});
+  useEffect(() => {
+    fetchProjectById(projectId, setProject);
+  }, []);
   return (
     <>
       <header>
-        <Navigation />
+        <Navigation
+          navigate={() => {
+            navigate("/dashboard");
+          }}
+        />
       </header>
       <>
-        <ProjectContent />
+        <ProjectContent
+          projectId={projectId}
+          projectName={project.projectName}
+        />
       </>
     </>
   );
