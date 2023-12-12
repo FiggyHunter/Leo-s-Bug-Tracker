ce0243fb04fcf282428c0bcd23368bdd8d037004
FiggyHunter <leonardo.roi3@gmail.com>
2023-12-12 23:50:15 +0100

    project card made dynamic

--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -1,17 +1,19 @@
-import { useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const ProjectCard = ({ name, description, color }) => {
+const ProjectCard = ({ name, description, color, id }) => {
   const divStyle = {
     backgroundColor: color,
   };
+
+  const navigate = useNavigate();
   return (
-    <article>
+    <article onClick={() => navigate(`/project/${id}`)}>
       <div
         style={divStyle}
         className={`lg:h-96 md:h-60 sm:h-36 w-full cursor-pointer  rounded-2xl`}
       ></div>
       <h3 className="text-content md:text-xl text-2xl font-onest font-medium cursor-pointer sm:text-base mt-3">
-        Project name bla bla blah Project name bla bla blah.
+        {name}
       </h3>
     </article>
   );
