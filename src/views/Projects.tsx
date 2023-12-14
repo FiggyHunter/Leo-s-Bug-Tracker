6e58eccbe280b5185b1dbfaeaba2505d9483ea54
FiggyHunter <leonardo.roi3@gmail.com>
2023-12-14 14:01:20 +0100

    implemented userId in projects

--- src/views/Projects.tsx
+++ src/views/Projects.tsx
@@ -4,12 +4,53 @@ import { useEffect, useState } from "react";
 import Navigation from "@/components/shared/Navigation";
 import { useJwt } from "react-jwt";
 import ProjectsContent from "@/components/projects/ProjectsContent";
+import { useIdStore } from "@/store/useUserId";
 
 const Projects = () => {
   const { jwt } = useCustomStore();
   const navigate = useNavigate();
   const [user, setUser] = useState({});
-  const token = useJwt(jwt) || null; // Directly use the token
+  const token = useJwt(jwt) || null;
+
+  const { userId, setUserId } = useIdStore();
+
+  useEffect(() => {
+    // Validate the jwt value obtained from the store
+    if (!jwt) {
+      console.log("No jwt, navigating to /login");
+      navigate("/login");
+      return;
+    }
+
+    if (token.isExpired) {
+      // Implement token refresh or logout if expired
+      navigate("/login");
+      return;
+    }
+
+    if (token.decodedToken) {
+      // Additional checks for decoded token properties
+      if (
+        token.decodedToken.Role === "unset" ||
+        token.decodedToken.Avatar === "unset"
+      ) {
+        console.log("Role or Avatar unset, navigating to /onboarding");
+        navigate("/onboarding");
+        return;
+      }
+
+      if (token.decodedToken.Name === "unset") {
+        console.log("Name unset, navigating to /onboarding-name");
+        navigate("/onboarding-name");
+        return;
+      }
+
+      // Set user and user ID
+      setUser(token.decodedToken);
+      setUserId(token.decodedToken.sub);
+      console.log(user);
+    }
+  }, [token.decodedToken]);
 
   return (
     <div className="h-my-screen grid custom-rows-2 grid-cols-1">
@@ -22,7 +63,7 @@ const Projects = () => {
         />
       </header>
       <main className="mt-8 mb-12 w-10/12 mx-auto px-4">
-        <ProjectsContent />
+        <ProjectsContent userName={user?.Name} userId={userId} />
       </main>
     </div>
   );
