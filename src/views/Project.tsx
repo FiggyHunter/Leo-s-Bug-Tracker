3b50a1db89d54c60e935077dcd67714cdf90d76b
FiggyHunter <leonardo.roi3@gmail.com>
2023-12-14 14:01:02 +0100

    prepared project for userId and token

--- src/views/Project.tsx
+++ src/views/Project.tsx
@@ -1,16 +1,23 @@
 import { fetchProjectById } from "@/api/projects/projects";
 import ProjectContent from "@/components/project/ProjectContent";
 import Navigation from "@/components/shared/Navigation";
+import { useCustomStore } from "@/store/useStore";
+import { useIdStore } from "@/store/useUserId";
 import { useEffect, useState } from "react";
+import { useJwt } from "react-jwt";
 import { useNavigate, useParams } from "react-router-dom";
 
 const Project = () => {
   const navigate = useNavigate();
   const params = useParams();
+
+  const { jwt } = useCustomStore();
+
   const { projectId } = params;
   const [project, setProject] = useState({});
+  const { userId } = useIdStore();
   useEffect(() => {
-    fetchProjectById(projectId, setProject);
+    fetchProjectById(projectId, setProject, userId);
   }, []);
   return (
     <>
