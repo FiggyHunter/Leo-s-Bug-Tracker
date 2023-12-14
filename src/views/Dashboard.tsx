0f1030472081eb136871e3041f58ce0fa36b5d00
FiggyHunter <leonardo.roi3@gmail.com>
2023-12-14 14:00:44 +0100

    prepared dashboard for the userId

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,48 +1,63 @@
 import DashboardContent from "@/components/dashboard/DashboardContent";
 import Navigation from "@/components/shared/Navigation";
 import { useCustomStore } from "@/store/useStore";
-import { useEffect, useState } from "react";
+import { useIdStore } from "@/store/useUserId";
+import { useCallback, useEffect, useState } from "react";
 import { useJwt } from "react-jwt";
 import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
   const { jwt } = useCustomStore();
+  const { userId, setUserId } = useIdStore();
   const navigate = useNavigate();
-
+  const token = useJwt(jwt);
   const [user, setUser] = useState({});
 
-  const token = useJwt(jwt) || null; // Directly use the token
-
-  console.log(token);
-
   useEffect(() => {
-    if (token.isExpired) {
+    // Validate the jwt value obtained from the store
+    if (!jwt) {
+      console.log("No jwt, navigating to /login");
       navigate("/login");
       return;
     }
-    if (token.decodedToken !== null) {
+
+    if (token.isExpired) {
+      // Implement token refresh or logout if expired
+      navigate("/login");
+      return;
+    }
+
+    if (token.decodedToken) {
+      // Additional checks for decoded token properties
       if (
         token.decodedToken.Role === "unset" ||
         token.decodedToken.Avatar === "unset"
       ) {
+        console.log("Role or Avatar unset, navigating to /onboarding");
         navigate("/onboarding");
         return;
       }
+
       if (token.decodedToken.Name === "unset") {
+        console.log("Name unset, navigating to /onboarding-name");
         navigate("/onboarding-name");
         return;
       }
-      setUser(token.decodedToken);
-    }
-  }, [token, navigate]);
 
+      // Set user and user ID
+      setUser(token.decodedToken);
+      setUserId(token.decodedToken.sub);
+    }
+  }, [token.decodedToken, userId]);
   return (
     <div className="h-my-screen grid custom-rows-2 grid-cols-1">
       <header>
         <Navigation avatar={`/${user?.Avatar}`} />
       </header>
       <main className="self-center mb-12 w-10/12 mx-auto">
-        {!token.isExpired && <DashboardContent userName={user?.Name} />}
+        {!token.isExpired && (
+          <DashboardContent userName={user?.Name} userId={userId} />
+        )}
       </main>
     </div>
   );
