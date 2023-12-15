d5e4a47339f98cd00fd4bf7fdd6524643d750a16
FiggyHunter <leonardo.roi3@gmail.com>
2023-12-15 19:44:01 +0100

    Style adjustments for light mode

--- src/components/onboarding/Role.tsx
+++ src/components/onboarding/Role.tsx
@@ -13,9 +13,9 @@ const Role: React.FC<Props> = ({ roleName, fullWidth, active, setRoles }) => {
   return (
     <button
       onClick={setRoles}
-      className={`w-1/2 h-full hover:text-black hover:bg-accent-1 hover:border-black  transition-all duration-250  border-white border-2 rounded-3xl grid place-items-center place-self-end md:text-2xl font-onest font-medium  ${
+      className={`w-1/2 text-content h-full hover:text-content hover:bg-accent-1 hover:border-black  transition-all duration-250  border-white border-2 rounded-3xl grid place-items-center place-self-end md:text-2xl font-onest font-medium  ${
         fullWidth ? "w-full" : "w-1/2"
-      } ${active ? activeStyle : "bg-transparent  text-white"}`}
+      } ${active ? activeStyle : "bg-transparent  text-content"}`}
     >
       {roleName}
     </button>
