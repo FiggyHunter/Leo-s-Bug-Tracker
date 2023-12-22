2427c73f3ae55f24fa851be3215415a0aba42a81
FiggyHunter <leonardo.roi3@gmail.com>
2023-12-22 23:50:25 +0100

    cleaned the projects component

--- src/views/Projects.tsx
+++ src/views/Projects.tsx
@@ -1,69 +1,27 @@
-import { useCustomStore } from "@/store/useStore";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Navigation from "@/components/shared/Navigation";
-import { useJwt } from "react-jwt";
 import ProjectsContent from "@/components/projects/ProjectsContent";
-import { useIdStore } from "@/store/useUserId";
+import useAuthToken from "@/hooks/useAuthToken";
 
 const Projects = () => {
-  const { jwt } = useCustomStore();
+  const { handleTokenCheck, userId, user, token } = useAuthToken();
   const navigate = useNavigate();
-  const [user, setUser] = useState({});
-  const token = useJwt(jwt) || null;
-
-  const { userId, setUserId } = useIdStore();
+  const fetchData = async () => {
+    await handleTokenCheck();
+  };
 
   useEffect(() => {
-    // Validate the jwt value obtained from the store
-    if (!jwt) {
-      console.log("No jwt, navigating to /login");
-      navigate("/login");
-      return;
-    }
-
-    if (token.isExpired) {
-      // Implement token refresh or logout if expired
-      navigate("/login");
-      return;
-    }
-
-    if (token.decodedToken) {
-      // Additional checks for decoded token properties
-      if (
-        token.decodedToken.Role === "unset" ||
-        token.decodedToken.Avatar === "unset"
-      ) {
-        console.log("Role or Avatar unset, navigating to /onboarding");
-        navigate("/onboarding");
-        return;
-      }
-
-      if (token.decodedToken.Name === "unset") {
-        console.log("Name unset, navigating to /onboarding-name");
-        navigate("/onboarding-name");
-        return;
-      }
-
-      // Set user and user ID
-      setUser(token.decodedToken);
-      setUserId(token.decodedToken.sub);
-      console.log(user);
-    }
-  }, [token.decodedToken]);
+    fetchData();
+  }, [userId, user, token]);
 
   return (
     <div className="h-my-screen grid custom-rows-2 grid-cols-1">
       <header>
-        <Navigation
-          navigate={() => {
-            navigate("/dashboard");
-          }}
-          avatar={`/${user?.Avatar}`}
-        />
+        <Navigation navigate={navigate} avatar={`/${user?.Avatar}`} />
       </header>
       <main className="mt-8 mb-12 w-10/12 mx-auto px-4">
-        <ProjectsContent userName={user?.Name} userId={userId} />
+        <ProjectsContent userName={user?.Name} userId={user?.sub} />
       </main>
     </div>
   );
