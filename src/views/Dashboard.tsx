6c6a0f044a9d63dd1496e0c6364786392aa883a3
FiggyHunter <leonardo.roi3@gmail.com>
2023-12-22 23:52:05 +0100

    refactored dashboard to use the new custom hook

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,51 +1,19 @@
 import DashboardContent from "@/components/dashboard/DashboardContent";
 import Navigation from "@/components/shared/Navigation";
-import { useCustomStore } from "@/store/useStore";
-import { useIdStore } from "@/store/useUserId";
-import { useCallback, useEffect, useState } from "react";
-import { useJwt } from "react-jwt";
+import useAuthToken from "@/hooks/useAuthToken";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
-  const { jwt } = useCustomStore();
-  const { userId, setUserId } = useIdStore();
+  const { handleTokenCheck, userId, user, token } = useAuthToken();
   const navigate = useNavigate();
-  const token = useJwt(jwt);
-  const [user, setUser] = useState({});
+  const fetchData = async () => {
+    await handleTokenCheck();
+  };
 
   useEffect(() => {
-    // Validate the jwt value obtained from the store
-    if (!jwt) {
-      navigate("/login");
-      return;
-    }
-
-    if (token.isExpired) {
-      // Implement token refresh or logout if expired
-      navigate("/login");
-      return;
-    }
-
-    if (token.decodedToken) {
-      // Additional checks for decoded token properties
-      if (
-        token.decodedToken.Role === "unset" ||
-        token.decodedToken.Avatar === "unset"
-      ) {
-        navigate("/onboarding");
-        return;
-      }
-
-      if (token.decodedToken.Name === "unset") {
-        navigate("/onboarding-name");
-        return;
-      }
-
-      // Set user and user ID
-      setUser(token.decodedToken);
-      setUserId(token.decodedToken.sub);
-    }
-  }, [token.decodedToken, userId]);
+    fetchData();
+  }, [userId, user, token]);
   return (
     <div className="h-my-screen grid custom-rows-2 grid-cols-1">
       <header>
