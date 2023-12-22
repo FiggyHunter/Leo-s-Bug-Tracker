5ba0f4bc64e75fe61b71c1e75462f391bc35619a
FiggyHunter <leonardo.roi3@gmail.com>
2023-12-22 23:52:36 +0100

    profile component now uses the new custom token hook

--- src/views/Profile.tsx
+++ src/views/Profile.tsx
@@ -1,50 +1,20 @@
 import ProfileContent from "@/components/profile/ProfileContent";
 import Navigation from "@/components/shared/Navigation";
-import { useCustomStore } from "@/store/useStore";
 import { useEffect, useState } from "react";
-import { useJwt } from "react-jwt";
 import { useNavigate } from "react-router-dom";
 import "@/styles/grid.css";
+import useAuthToken from "@/hooks/useAuthToken";
 
 const Profile = () => {
-  const { jwt } = useCustomStore();
-  const [user, setUser] = useState({});
-  const token = useJwt(jwt) || null;
+  const { handleTokenCheck, userId, user, token } = useAuthToken();
   const navigate = useNavigate();
-  console.log(token);
+  const fetchData = async () => {
+    await handleTokenCheck();
+  };
 
   useEffect(() => {
-    // Validate the jwt value obtained from the store
-    if (!jwt) {
-      navigate("/login");
-      return;
-    }
-
-    if (token.isExpired) {
-      // Implement token refresh or logout if expired
-      navigate("/login");
-      return;
-    }
-
-    if (token.decodedToken) {
-      // Additional checks for decoded token properties
-      if (
-        token.decodedToken.Role === "unset" ||
-        token.decodedToken.Avatar === "unset"
-      ) {
-        navigate("/onboarding");
-        return;
-      }
-
-      if (token.decodedToken.Name === "unset") {
-        navigate("/onboarding-name");
-        return;
-      }
-
-      // Set user and user ID
-      setUser(token.decodedToken);
-    }
-  }, [token.decodedToken]);
+    fetchData();
+  }, [userId, user, token]);
 
   return (
     <div className="h-my-screen grid  grid-rows-custom">
